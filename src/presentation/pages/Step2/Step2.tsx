f6a72f5d5fce27cafd7bfc9bdadb9aea83b912e5
岩原公貴 <iwaharakouki@iwaharaakanoMBP.lan>
2020-06-17 21:23:02 +0900

    Step2ページのデータをstateに格納

--- src/presentation/pages/Step2/Step2.tsx
+++ src/presentation/pages/Step2/Step2.tsx
@@ -1,21 +1,49 @@
 import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
+import { ChangeEventHandler } from '../../container/Step2Container'
+import { State } from '../../../store/reducer'
 import Header from '../../components/organisms/Header/Header'
 import RadioButton from '../../components/atoms/Radio/RadioButton'
 import Button from '../../components/atoms/Button/Button'
+import '../../styles/main.css'
 
-const Step2: React.FC = () => {
+const Step2: React.FC<State & ChangeEventHandler> = (props) => {
   const history = useHistory()
   const [isSecondQuestion, setIsSecondQuestion] = useState(false)
   const [isThirdQuestion, setIsThirdQuestion] = useState(false)
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const { question1, question2, question3, handleOnChangeValue } = { ...props }
+  const showQuestion = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.name === '質問1' && !isSecondQuestion) {
       setIsSecondQuestion(true)
     } else if (e.target.name === '質問2' && !isThirdQuestion) {
       setIsThirdQuestion(true)
     }
   }
+  const changeCustomerValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const customerInfo = {
+      question1,
+      question2,
+      question3,
+    }
+    switch (e.target.name) {
+      case '質問1':
+        customerInfo.question1 = e.target.value
+        handleOnChangeValue(customerInfo)
+        break
+      case '質問2':
+        customerInfo.question2 = e.target.value
+        handleOnChangeValue(customerInfo)
+        break
+      case '質問3':
+        customerInfo.question3 = e.target.value
+        handleOnChangeValue(customerInfo)
+        break
+      default:
+        return customerInfo
+    }
+    showQuestion(e)
+  }
+  console.log(props)
   return (
     <>
       <Header label="STEP2" text="以下にお答えください"></Header>
@@ -24,13 +52,13 @@ const Step2: React.FC = () => {
         name={'質問1'}
         value={'はい'}
         id={'q1Yes'}
-        onChange={(event) => handleChange(event)}
+        onChange={(event) => changeCustomerValue(event)}
       ></RadioButton>
       <RadioButton
         name={'質問1'}
         value={'いいえ'}
         id={'q1No'}
-        onChange={(event) => handleChange(event)}
+        onChange={(event) => changeCustomerValue(event)}
       ></RadioButton>
       <div className={isSecondQuestion ? '' : 'hidden'}>
         <div>現在入院中ですか？3ヶ月以内に手術をすすめられましたか？</div>
@@ -38,13 +66,13 @@ const Step2: React.FC = () => {
           name={'質問2'}
           value={'はい'}
           id={'q2Yes'}
-          onChange={(event) => handleChange(event)}
+          onChange={(event) => changeCustomerValue(event)}
         ></RadioButton>
         <RadioButton
           name={'質問2'}
           value={'いいえ'}
           id={'q2No'}
-          onChange={(event) => handleChange(event)}
+          onChange={(event) => changeCustomerValue(event)}
         ></RadioButton>
       </div>
       <div className={isThirdQuestion ? '' : 'hidden'}>
@@ -53,13 +81,13 @@ const Step2: React.FC = () => {
           name={'質問3'}
           value={'はい'}
           id={'q3Yes'}
-          onChange={(event) => console.log(event.target.value)}
+          onChange={(event) => changeCustomerValue(event)}
         ></RadioButton>
         <RadioButton
           name={'質問3'}
           value={'いいえ'}
           id={'q3No'}
-          onChange={(event) => console.log(event.target.value)}
+          onChange={(event) => changeCustomerValue(event)}
         ></RadioButton>
       </div>
 
