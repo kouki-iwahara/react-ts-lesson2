44b751cf3f4b1b709aa205fef1fac8bc870d7081
岩原公貴 <iwaharakouki@iwaharaakanoMBP.lan>
2020-06-17 21:15:01 +0900

    textAreaコンポーネント作成

--- src/presentation/components/atoms/TextArea/TextArea.tsx
+++ src/presentation/components/atoms/TextArea/TextArea.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
 
-const TextArera: React.FC = () => {
-  return <textarea name="" id=""></textarea>
+type TProp = {
+  value: string
+  onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void
+}
+
+const TextArera: React.FC<TProp> = (props) => {
+  const { value, onChange } = { ...props }
+
+  return (
+    <textarea
+      name=""
+      id=""
+      defaultValue={value}
+      onChange={(event) => onChange(event)}
+    ></textarea>
+  )
 }
 
 export default TextArera
